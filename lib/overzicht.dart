f0b1869d26756c4dbca0051ba2c108447a92cfaf
Rob <505067@student.fontys.nl>
2024-05-24 14:21:56 +0200

    added graph with base data and area for flower

--- lib/overzicht.dart
+++ lib/overzicht.dart
@@ -1,7 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
 
-class OverzichtPage extends StatelessWidget {
+class OverzichtPage extends StatefulWidget {
   const OverzichtPage({super.key});
+
+  @override
+  State<OverzichtPage> createState() => _OverzichtPageState();
+}
+
+class _OverzichtPageState extends State<OverzichtPage> {
+  List<_SalesData> data = [
+    _SalesData('Jan', 1),
+    _SalesData('Feb', 3),
+    _SalesData('Mar', 4),
+    _SalesData('Apr', 3),
+    _SalesData('May', 4),
+    _SalesData('Jun', 4),
+    _SalesData('Jul', 5),
+    _SalesData('Aug', 6),
+    _SalesData('Sep', 7),
+    _SalesData('Oct', 8),
+    _SalesData('Nov', 9),
+    _SalesData('Dec', 10)
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -9,9 +30,41 @@ class OverzichtPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Overzicht Page'),
       ),
-      body: const Center(
-        child: Text('Hello World!'),
-      ),
+      body: Column(children: [
+        Text('Onderwerpen hier'),
+        SizedBox(height: 20),
+        Container(
+          width: 300,
+          height: 300,
+          color: Colors.red,
+        ),
+        SizedBox(height: 20),
+        SfCartesianChart(
+          primaryXAxis: CategoryAxis(),
+          primaryYAxis: const NumericAxis(
+            interval: 1, // Specify the interval between labels
+            minimum: 0, // Set the minimum value of the axis
+            maximum: 10, // Set the maximum value of the axis
+          ),
+          tooltipBehavior: TooltipBehavior(enable: true),
+          series: <CartesianSeries<_SalesData, String>>[
+            LineSeries<_SalesData, String>(
+              dataSource: data,
+              xValueMapper: (_SalesData sales, _) => sales.year,
+              yValueMapper: (_SalesData sales, _) => sales.sales,
+              name: 'Sales',
+              markerSettings: MarkerSettings(isVisible: true),
+            ),
+          ],
+        )
+      ]),
     );
   }
+}
+
+class _SalesData {
+  _SalesData(this.year, this.sales);
+
+  final String year;
+  final double sales;
 }
