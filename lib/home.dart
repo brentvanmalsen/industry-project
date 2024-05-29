9cd0dc3677e9d13fa5740fd3f0fda40114ebc261
JorgRijdt <jorgvanderijdt5@gmail.com>
2024-05-29 09:51:14 +0200

    test

--- lib/home.dart
+++ lib/home.dart
@@ -1,27 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:share_plus/share_plus.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home Page'),
-        actions: [
-          IconButton(
-            icon: const Icon(Icons.person_add),
-            tooltip: 'Invite People',
-            onPressed: () {
-              final String inviteLink = 'https://yourapp.com/invite';
-              Share.share('Join our app using this link: $inviteLink');
-            },
-          ),
-        ],
-      ),
-      body: Center(
-        child: const Text('Welcome to Home Page!'),
+    return const MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Text('Hello World!'),
+        ),
       ),
     );
   }
