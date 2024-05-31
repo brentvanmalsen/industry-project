6e3e0634fd696bb5cee6657b9d8cb83ecc6a54cd
Koen Hilbrands <I519462@fhict.nl519462@student.fontys.nl>
2024-05-31 11:48:00 +0200

    added settings

--- lib/overview.dart
+++ lib/overview.dart
@@ -1,19 +1,14 @@
 import 'package:flutter/material.dart';
 
-void overzicht() {
-  runApp(const OverzichtPage());
-}
-
-class OverzichtPage extends StatelessWidget {
-  const OverzichtPage({super.key});
-
+class OverviewPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Overzichten'),
+      ),
+      body: Center(
+        child: Text('Welcome to Overview Page'),
       ),
     );
   }
