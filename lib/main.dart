036ab4bf41e4f04fd03560deab4cf1e2646d3017
JorgRijdt <jorgvanderijdt5@gmail.com>
2024-06-06 09:49:51 +0200

    skip knop toegevoegd die meteen gaat naar home

--- lib/main.dart
+++ lib/main.dart
@@ -27,56 +27,80 @@ class _InfoScreenState extends State<InfoScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
+      body: Stack(
         children: [
-          Expanded(
-            child: PageView(
-              controller: _controller,
-              children: <Widget>[
-                _buildPage(
-                  context,
-                  'assets/logo.png',
-                  'Welkom bij PeacePath',
-                  'Hier krijg je een korte intrductie over de app.',
+          Column(
+            children: [
+              Expanded(
+                child: PageView(
+                  controller: _controller,
+                  children: <Widget>[
+                    _buildPage(
+                      context,
+                      'assets/logo.png',
+                      'Welkom bij PeacePath',
+                      'De app om je overstimulatie te monitoren en inzichten te geven.',
+                    ),
+                    _buildPage(
+                      context,
+                      'assets/begin.png',
+                      'Communiceer',
+                      'Als je last hebt van overstimulatie laat dit weten door op de grote knop te drukken op de home pagina.',
+                    ),
+                    _buildPage(
+                      context,
+                      'assets/invullen.png',
+                      'Invullen',
+                      'Vul vervolgens in bij welk onderwerp dit hoorde zoals: stad, werk, sport etc, of voeg een eigen onderwerp toe.',
+                    ),
+                    _buildPage(
+                      context,
+                      'assets/grafiek.png',
+                      'Overzicht',
+                      'Bekijk je inzichten en evalueer elke keer hoe je je voelde. Probeer patronen te herkennen en kijk of er bepaalde momenten, plaatsen of activiteiten zijn die je overstimuleren.',
+                    ),
+                    _buildPage(
+                      context,
+                      'assets/starten.png',
+                      'Get Started',
+                      'Let\'s get started!',
+                      showButton: true,
+                    ),
+                  ],
                 ),
-                _buildPage(
-                  context,
-                  'assets/begin.png',
-                  'Features',
-                  'ALs je last hebt van overstimulatie laat dit weten door op de grote knop te drukken op de home pagina.',
+              ),
+              SmoothPageIndicator(
+                controller: _controller,
+                count: 5,
+                effect: WormEffect(
+                  dotHeight: 10.0,
+                  dotWidth: 10.0,
+                  activeDotColor: Colors.blue,
                 ),
-                _buildPage(
+              ),
+              SizedBox(height: 20),
+            ],
+          ),
+          Positioned(
+            bottom: 20,
+            right: 20,
+            child: GestureDetector(
+              onTap: () {
+                Navigator.pushReplacement(
                   context,
-                  'assets/invullen.png',
-                  'Invullen',
-                  'Vul vervolgens in bij welk onderwerp dit hoorde zoals: stad, werk, sport etc, of voeg een eigen onderwerp toe.',
+                  MaterialPageRoute(builder: (context) => HomeScreen()),
+                );
+              },
+              child: Text(
+                'Skip',
+                style: TextStyle(
+                  decoration: TextDecoration.underline,
+                  fontSize: 16,
+                  color: Color(0xFF4182DB),
                 ),
-                _buildPage(
-                  context,
-                  'assets/grafiek.png',
-                  'Overzicht',
-                  'Bekijk je overzicht',
-                ),
-                _buildPage(
-                  context,
-                  'assets/grafiek.png',
-                  'Get Started',
-                  'Let\'s get started!',
-                  showButton: true,
-                ),
-              ],
+              ),
             ),
           ),
-          SmoothPageIndicator(
-            controller: _controller,
-            count: 5,
-            effect: WormEffect(
-              dotHeight: 10.0,
-              dotWidth: 10.0,
-              activeDotColor: Colors.blue,
-            ),
-          ),
-          SizedBox(height: 20),
         ],
       ),
     );
@@ -89,7 +113,7 @@ class _InfoScreenState extends State<InfoScreen> {
       mainAxisAlignment: MainAxisAlignment.center,
       children: <Widget>[
         Image.asset(
-          imagePath, // Make sure to add your images to the assets folder
+          imagePath,
           width: 350,
           height: 350,
         ),
@@ -109,14 +133,30 @@ class _InfoScreenState extends State<InfoScreen> {
         ),
         if (showButton) SizedBox(height: 20),
         if (showButton)
-          ElevatedButton(
-            onPressed: () {
-              Navigator.pushReplacement(
-                context,
-                MaterialPageRoute(builder: (context) => HomeScreen()),
-              );
-            },
-            child: Text('Get Started'),
+          SizedBox(
+            width: 325, // Make the button wider
+            height: 50, // Make the button taller
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Color(0xFF4182DB), // Background color
+                foregroundColor: Colors.white, // Text color
+                textStyle: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold, // Make the text bold
+                ),
+                shape: RoundedRectangleBorder(
+                  borderRadius:
+                      BorderRadius.circular(10), // Border radius of 10
+                ),
+              ),
+              onPressed: () {
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(builder: (context) => HomeScreen()),
+                );
+              },
+              child: Text('Get Started'),
+            ),
           ),
       ],
     );
